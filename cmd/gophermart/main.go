c40285e5f6ccb555d1ce774948031d1fa4f6e750
Dmitry Romanenko <letisflywithme@icloud.com>
2024-11-13 10:10:58 +0300

    fix first

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,57 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Config struct {
+	Port string `default:"8080"`
+}
+
+type Application struct {
+	Config Config
+	//Model
+}
+
+func (app *Application) Serve() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
+	fmt.Printf("Listening on port %s\n", port)
+
+	srv := &http.Server{
+		Addr: fmt.Sprintf(":%s" + port),
+		// TODO: add router
+	}
+	return srv.ListenAndServe()
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cfg := Config{
+		Port: os.Getenv("PORT"),
+	}
+
+	// TODO: connection to db
+
+	app := &Application{
+		Config: cfg,
+		// TODO: add models later
+	}
+
+	err = app.Serve()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
